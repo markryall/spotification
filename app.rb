0837d20a9436d741b949f90f0a22ae408204c6ef
Mark Ryall <mark@ryall.name>
2012-11-25 14:49:47 +1000

    renamed search to track

--- app.rb
+++ app.rb
@@ -1,16 +1,19 @@
 require 'sinatra'
 require 'slim'
 require 'httparty'
+require 'cgi'
 
 get '/lastfm/:user' do |user|
   tracks = HTTParty.get "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying=true&user=#{user}&api_key=#{ENV['LASTFM_API_KEY']}&format=json"
   slim :index, locals: {tracks: tracks['recenttracks']['track']}
 end
 
-get '/search' do
-  slim :search
+get '/tracks' do
+  slim :search, locals: {criteria: '', tracks: []}
 end
 
-post '/search' do
-  slim :results
+post '/tracks' do
+  criteria = params[:criteria]
+  results = HTTParty.get "http://ws.spotify.com/search/1/track.json?q=#{CGI.escape criteria}"
+  slim :search, locals: {criteria: criteria, tracks: results['tracks']}
 end
