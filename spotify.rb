f15be693f30cf7ae12e03643f4aa2cdeb67348a5
Mark Ryall <mark@ryall.name>
2012-11-25 16:33:05 +1000

    added result filtering by spotify territory

--- spotify.rb
+++ spotify.rb
@@ -1,9 +1,16 @@
 require 'httparty'
 require 'cgi'
 
+unless ENV['SPOTIFY_TERRITORY']
+  puts 'please set SPOTIFY_TERRITORY (eg. AU)'
+  exit 1
+end
+
 module Spotify
   def spotify_tracks_matching criteria
     response = HTTParty.get "http://ws.spotify.com/search/1/track.json?q=#{CGI.escape criteria}"
-    response['tracks']
+    response['tracks'].find_all do |track|
+      track['album']['availability']['territories'].split.include? ENV['SPOTIFY_TERRITORY']
+    end
   end
 end
