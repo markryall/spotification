789168fbbe3946a13a078ec24845fc5450b5cc6a
Mark Ryall <mark@ryall.name>
2012-11-25 14:35:33 +1000

    split out layout

--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ get '/lastfm/:user' do |user|
   tracks = HTTParty.get "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying=true&user=#{user}&api_key=#{ENV['LASTFM_API_KEY']}&format=json"
   slim :index, locals: {tracks: tracks['recenttracks']['track']}
 end
+
+get '/search' do
+  slim :search
+end
+
+post '/search' do
+  slim :results
+end
