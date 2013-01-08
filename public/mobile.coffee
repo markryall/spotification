5c372e2c348f33f2c077ebf0b7350dcfc661e5ee
Mark Ryall <mark@ryall.name>
2013-01-08 23:41:34 +1000

    added dequeing of tracks to queue

--- public/mobile.coffee
+++ public/mobile.coffee
@@ -1,22 +1,33 @@
 $ ->
   queue_template = """
-  <ul id="queue-list" data-role="listview">
+  <ul id="queue-list" data-role="listview" data-split-icon="delete" data-split-theme="d">
     {{#tracks}}
       <li>
-        <img src="{{icon}}" class="ui-li-thumb">
-        <h3 class="ui-li-heading">{{name}} ({{duration}})</h3>
-        <p class="ui-li-desc">{{album}}</p>
-        <p class="ui-li-desc">{{artists}}</p>
+        <a href="#">
+          <img src="{{icon}}" class="ui-li-thumb">
+          <h3 class="ui-li-heading">{{name}} ({{duration}})</h3>
+          <p class="ui-li-desc">{{album}}</p>
+          <p class="ui-li-desc">{{artists}}</p>
+        </a>
+        <a href="#" class='remove' data-track-index="{{index}}">remove</a>
       </li>
     {{/tracks}}
   </ul>
   """
+
+  remove_queued_track = ->
+    remove = $(this).parent()
+    $.post '/dequeue',
+      id: $(this).data('track-index'),
+      success: ->
+        remove.slideUp()
 
   load_queue = ->
     $.get '/api/queue', {}, (data) ->
       list = Mustache.to_html queue_template, data
       $('#queue-tracks').html list
       $('#queue-list').listview()
+      $('#queue-tracks a.remove').click remove_queued_track
 
   $('div').live 'pagehide', (event, ui) ->
     switch $(ui.nextPage).attr('id')
--- public/mobile.js
+++ public/mobile.js
@@ -1,14 +1,25 @@
 (function() {
 
   $(function() {
-    var load_queue, queue_template;
-    queue_template = "<ul id=\"queue-list\" data-role=\"listview\">\n  {{#tracks}}\n    <li>\n      <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n      <h3 class=\"ui-li-heading\">{{name}} ({{duration}})</h3>\n      <p class=\"ui-li-desc\">{{album}}</p>\n      <p class=\"ui-li-desc\">{{artists}}</p>\n    </li>\n  {{/tracks}}\n</ul>";
+    var load_queue, queue_template, remove_queued_track;
+    queue_template = "<ul id=\"queue-list\" data-role=\"listview\" data-split-icon=\"delete\" data-split-theme=\"d\">\n  {{#tracks}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}} ({{duration}})</h3>\n        <p class=\"ui-li-desc\">{{album}}</p>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n      </a>\n      <a href=\"#\" class='remove' data-track-index=\"{{index}}\">remove</a>\n    </li>\n  {{/tracks}}\n</ul>";
+    remove_queued_track = function() {
+      var remove;
+      remove = $(this).parent();
+      return $.post('/dequeue', {
+        id: $(this).data('track-index'),
+        success: function() {
+          return remove.slideUp();
+        }
+      });
+    };
     load_queue = function() {
       return $.get('/api/queue', {}, function(data) {
         var list;
         list = Mustache.to_html(queue_template, data);
         $('#queue-tracks').html(list);
-        return $('#queue-list').listview();
+        $('#queue-list').listview();
+        return $('#queue-tracks a.remove').click(remove_queued_track);
       });
     };
     $('div').live('pagehide', function(event, ui) {
