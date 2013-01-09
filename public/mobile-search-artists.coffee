55f407e72349e6947d889b4a7430895961ca9911
Mark Ryall <mark@ryall.name>
2013-01-09 21:32:49 +1000

    added display of artist search results

--- public/mobile-search-artists.coffee
+++ public/mobile-search-artists.coffee
@@ -1,9 +1,23 @@
 $ ->
+  template = """
+  <p>{{info.num_results}} artists</p>
+  <ul id="albums-list" data-role="listview">
+    {{#artists}}
+      <li>
+        <a href="#" data-track-id="{{id}}">{{name}}</a>
+      </li>
+    {{/artists}}
+  </ul>
+  """
+
   $('#search-artists-form').submit ->
     $.mobile.loading 'show'
     $.get '/api/search/artists',
       criteria: $('#search-artists-criteria').val(),
       (data) ->
         $.mobile.loading 'hide'
-        console.log data
+        content = Mustache.to_html template, data
+        $('#artists .artists-content').html content
+        $.mobile.changePage '#artists'
+        $('#artists-list').listview()
     false
--- public/mobile-search-artists.js
+++ public/mobile-search-artists.js
@@ -1,13 +1,19 @@
 (function() {
 
   $(function() {
+    var template;
+    template = "<p>{{info.num_results}} artists</p>\n<ul id=\"albums-list\" data-role=\"listview\">\n  {{#artists}}\n    <li>\n      <a href=\"#\" data-track-id=\"{{id}}\">{{name}}</a>\n    </li>\n  {{/artists}}\n</ul>";
     return $('#search-artists-form').submit(function() {
       $.mobile.loading('show');
       $.get('/api/search/artists', {
         criteria: $('#search-artists-criteria').val()
       }, function(data) {
+        var content;
         $.mobile.loading('hide');
-        return console.log(data);
+        content = Mustache.to_html(template, data);
+        $('#artists .artists-content').html(content);
+        $.mobile.changePage('#artists');
+        return $('#artists-list').listview();
       });
       return false;
     });
