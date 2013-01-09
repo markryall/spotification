29a934c9ebf3f64d93878b2a39cb3460ce0f22b3
Mark Ryall <mark@ryall.name>
2013-01-10 08:38:05 +1000

    refactored rendering of tracks into a function

--- public/mobile-search-tracks.coffee
+++ public/mobile-search-tracks.coffee
@@ -1,37 +1,40 @@
 $ ->
-  template = """
-  <p>{{info.num_results}} tracks</p>
-  <ul id="tracks-list" data-role="listview" data-split-icon="plus" data-split-theme="d">
-    {{#tracks}}
-      <li>
-        <a href="#">
-          <img src="{{icon}}" class="ui-li-thumb">
-          <h3 class="ui-li-heading">{{name}} ({{duration}})</h3>
-          <p class="ui-li-desc">{{album}}</p>
-          <p class="ui-li-desc">{{artists}}</p>
-        </a>
-        <a href="#" class='plus' data-track-id="{{id}}">remove</a>
-      </li>
-    {{/tracks}}
-  </ul>
-  """
+  show_tracks = (data) ->
+    template = """
+    <p>{{title}}</p>
+    <ul id="tracks-list" data-role="listview" data-split-icon="plus" data-split-theme="d">
+      {{#tracks}}
+        <li>
+          <a href="#">
+            <img src="{{icon}}" class="ui-li-thumb">
+            <h3 class="ui-li-heading">{{name}} ({{duration}})</h3>
+            <p class="ui-li-desc">{{album}}</p>
+            <p class="ui-li-desc">{{artists}}</p>
+          </a>
+          <a href="#" class='plus' data-track-id="{{id}}">remove</a>
+        </li>
+      {{/tracks}}
+    </ul>
+    """
 
-  queue_track = ->
-    remove = $(this).parent()
-    $.post '/api/enqueue/track',
-      id: $(this).data('track-id'),
-      success: ->
-        remove.slideUp()
+    queue_track = ->
+      remove = $(this).parent()
+      $.post '/api/enqueue/track',
+        id: $(this).data('track-id'),
+        success: ->
+          remove.slideUp()
+
+    content = Mustache.to_html template, data
+    $('#tracks .tracks-content').html content
+    $.mobile.changePage '#tracks'
+    $('#tracks-list').listview()
+    $('#tracks-list a.plus').click queue_track
 
   $('#search-tracks-form').submit ->
     $.mobile.loading 'show'
     $.get '/api/search/tracks',
       criteria: $('#search-tracks-criteria').val(),
       (data) ->
-        content = Mustache.to_html template, data
-        $('#tracks .tracks-content').html content
         $.mobile.loading 'hide'
-        $.mobile.changePage '#tracks'
-        $('#tracks-list').listview()
-        $('#tracks-list a.plus').click queue_track
+        show_tracks tracks: data.tracks, title: "#{data.info.num_results} tracks"
     false
--- public/mobile-search-tracks.js
+++ public/mobile-search-tracks.js
@@ -1,30 +1,36 @@
 (function() {
 
   $(function() {
-    var queue_track, template;
-    template = "<p>{{info.num_results}} tracks</p>\n<ul id=\"tracks-list\" data-role=\"listview\" data-split-icon=\"plus\" data-split-theme=\"d\">\n  {{#tracks}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}} ({{duration}})</h3>\n        <p class=\"ui-li-desc\">{{album}}</p>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n      </a>\n      <a href=\"#\" class='plus' data-track-id=\"{{id}}\">remove</a>\n    </li>\n  {{/tracks}}\n</ul>";
-    queue_track = function() {
-      var remove;
-      remove = $(this).parent();
-      return $.post('/api/enqueue/track', {
-        id: $(this).data('track-id'),
-        success: function() {
-          return remove.slideUp();
-        }
-      });
+    var show_tracks;
+    show_tracks = function(data) {
+      var content, queue_track, template;
+      template = "<p>{{title}}</p>\n<ul id=\"tracks-list\" data-role=\"listview\" data-split-icon=\"plus\" data-split-theme=\"d\">\n  {{#tracks}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}} ({{duration}})</h3>\n        <p class=\"ui-li-desc\">{{album}}</p>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n      </a>\n      <a href=\"#\" class='plus' data-track-id=\"{{id}}\">remove</a>\n    </li>\n  {{/tracks}}\n</ul>";
+      queue_track = function() {
+        var remove;
+        remove = $(this).parent();
+        return $.post('/api/enqueue/track', {
+          id: $(this).data('track-id'),
+          success: function() {
+            return remove.slideUp();
+          }
+        });
+      };
+      content = Mustache.to_html(template, data);
+      $('#tracks .tracks-content').html(content);
+      $.mobile.changePage('#tracks');
+      $('#tracks-list').listview();
+      return $('#tracks-list a.plus').click(queue_track);
     };
     return $('#search-tracks-form').submit(function() {
       $.mobile.loading('show');
       $.get('/api/search/tracks', {
         criteria: $('#search-tracks-criteria').val()
       }, function(data) {
-        var content;
-        content = Mustache.to_html(template, data);
-        $('#tracks .tracks-content').html(content);
         $.mobile.loading('hide');
-        $.mobile.changePage('#tracks');
-        $('#tracks-list').listview();
-        return $('#tracks-list a.plus').click(queue_track);
+        return show_tracks({
+          tracks: data.tracks,
+          title: "" + data.info.num_results + " tracks"
+        });
       });
       return false;
     });
