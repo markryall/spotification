27f1fedc88e2e99783577b12df54fadf6bcdc33a
Mark Ryall <mark@ryall.name>
2013-01-09 20:02:52 +1000

    loading track search results on a track page

--- public/mobile-search-tracks.coffee
+++ public/mobile-search-tracks.coffee
@@ -1,10 +1,29 @@
 $ ->
+  template = """
+  <p>{{info.num_results}} tracks</p>
+  <ul id="tracks-list" data-role="listview" data-split-icon="plus" data-split-theme="d">
+    {{#tracks}}
+      <li>
+        <a href="#">
+          <img src="{{icon}}" class="ui-li-thumb">
+          <h3 class="ui-li-heading">{{name}} ({{duration}})</h3>
+          <p class="ui-li-desc">{{album}}</p>
+          <p class="ui-li-desc">{{artists}}</p>
+        </a>
+        <a href="#" class='plus' data-track-index="{{id}}">remove</a>
+      </li>
+    {{/tracks}}
+  </ul>
+  """
+
   $('#search-tracks-form').submit ->
     $.mobile.loading 'show'
     $.get '/api/search/tracks',
       criteria: $('#search-tracks').val(),
       (data) ->
-        console.debug data
+        content = Mustache.to_html template, data
+        $('#tracks .tracks-content').html content
+        $('#tracks-list').listview()
         $.mobile.loading 'hide'
         $.mobile.changePage '#tracks'
     false
--- public/mobile-search-tracks.js
+++ public/mobile-search-tracks.js
@@ -1,12 +1,17 @@
 (function() {
 
   $(function() {
+    var template;
+    template = "<p>{{info.num_results}} tracks</p>\n<ul id=\"tracks-list\" data-role=\"listview\" data-split-icon=\"plus\" data-split-theme=\"d\">\n  {{#tracks}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}} ({{duration}})</h3>\n        <p class=\"ui-li-desc\">{{album}}</p>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n      </a>\n      <a href=\"#\" class='plus' data-track-index=\"{{id}}\">remove</a>\n    </li>\n  {{/tracks}}\n</ul>";
     return $('#search-tracks-form').submit(function() {
       $.mobile.loading('show');
       $.get('/api/search/tracks', {
         criteria: $('#search-tracks').val()
       }, function(data) {
-        console.debug(data);
+        var content;
+        content = Mustache.to_html(template, data);
+        $('#tracks .tracks-content').html(content);
+        $('#tracks-list').listview();
         $.mobile.loading('hide');
         return $.mobile.changePage('#tracks');
       });
