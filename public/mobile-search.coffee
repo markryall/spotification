dbcbd46071400cf7f47b3b5fb348842f488399f6
Mark Ryall <mark@ryall.name>
2013-01-10 08:49:34 +1000

    moved all search coffeescript into a single file

--- public/mobile-search.coffee
+++ public/mobile-search.coffee
@@ -39,6 +39,46 @@ $ ->
         show_tracks tracks: data.tracks, title: "#{data.info.num_results} tracks"
     false
 
+  show_albums = (data) ->
+    template = """
+    <p>{{title}}</p>
+    <ul id="albums-list" data-role="listview" data-split-icon="plus" data-split-theme="d">
+      {{#albums}}
+        <li>
+          <a href="#">
+            <img src="{{icon}}" class="ui-li-thumb">
+            <h3 class="ui-li-heading">{{name}}</h3>
+            <p class="ui-li-desc">{{artists}}</p>
+            <p class="ui-li-desc">{{date}} - {{count}} tracks - {{duration}}</p>
+          </a>
+          <a href="#" class='plus' data-album-id="{{id}}">add</a>
+        </li>
+      {{/albums}}
+    </ul>
+    """
+
+    queue_album = ->
+      remove = $(this).parent()
+      $.post '/api/enqueue/album',
+        id: $(this).data('album-id'),
+        success: ->
+          remove.slideUp()
+
+    content = Mustache.to_html template, data
+    $('#albums .albums-content').html content
+    $.mobile.changePage '#albums'
+    $('#albums-list').listview()
+    $('#albums-list a.plus').click queue_album
+
+  $('#search-albums-form').submit ->
+    $.mobile.loading 'show'
+    $.get '/api/search/albums',
+      criteria: $('#search-albums-criteria').val(),
+      (data) ->
+        $.mobile.loading 'hide'
+        show_albums albums: data.albums, title: "#{data.info.num_results} albums"
+    false
+
   show_artists = (data) ->
     template = """
     <p>{{title}}</p>
@@ -62,5 +102,5 @@ $ ->
       criteria: $('#search-artists-criteria').val(),
       (data) ->
         $.mobile.loading 'hide'
-        show_artists artists: data.artists, title: "#{data.info.num_results} albums"
+        show_artists artists: data.artists, title: "#{data.info.num_results} artists"
     false
--- public/mobile-search.js
+++ public/mobile-search.js
@@ -1,7 +1,7 @@
 (function() {
 
   $(function() {
-    var show_artists, show_tracks;
+    var show_albums, show_artists, show_tracks;
     show_tracks = function(data) {
       var content, queue_track, template;
       template = "<p>{{title}}</p>\n<ul id=\"tracks-list\" data-role=\"listview\" data-split-icon=\"plus\" data-split-theme=\"d\">\n  {{#tracks}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}} ({{duration}})</h3>\n        <p class=\"ui-li-desc\">{{album}}</p>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n      </a>\n      <a href=\"#\" class='plus' data-track-id=\"{{id}}\">remove</a>\n    </li>\n  {{/tracks}}\n</ul>";
@@ -34,6 +34,38 @@
       });
       return false;
     });
+    show_albums = function(data) {
+      var content, queue_album, template;
+      template = "<p>{{title}}</p>\n<ul id=\"albums-list\" data-role=\"listview\" data-split-icon=\"plus\" data-split-theme=\"d\">\n  {{#albums}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}}</h3>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n        <p class=\"ui-li-desc\">{{date}} - {{count}} tracks - {{duration}}</p>\n      </a>\n      <a href=\"#\" class='plus' data-album-id=\"{{id}}\">add</a>\n    </li>\n  {{/albums}}\n</ul>";
+      queue_album = function() {
+        var remove;
+        remove = $(this).parent();
+        return $.post('/api/enqueue/album', {
+          id: $(this).data('album-id'),
+          success: function() {
+            return remove.slideUp();
+          }
+        });
+      };
+      content = Mustache.to_html(template, data);
+      $('#albums .albums-content').html(content);
+      $.mobile.changePage('#albums');
+      $('#albums-list').listview();
+      return $('#albums-list a.plus').click(queue_album);
+    };
+    $('#search-albums-form').submit(function() {
+      $.mobile.loading('show');
+      $.get('/api/search/albums', {
+        criteria: $('#search-albums-criteria').val()
+      }, function(data) {
+        $.mobile.loading('hide');
+        return show_albums({
+          albums: data.albums,
+          title: "" + data.info.num_results + " albums"
+        });
+      });
+      return false;
+    });
     show_artists = function(data) {
       var content, template;
       template = "<p>{{title}}</p>\n<ul id=\"albums-list\" data-role=\"listview\">\n  {{#artists}}\n    <li>\n      <a href=\"#\" data-track-id=\"{{id}}\">{{name}}</a>\n    </li>\n  {{/artists}}\n</ul>";
@@ -50,7 +82,7 @@
         $.mobile.loading('hide');
         return show_artists({
           artists: data.artists,
-          title: "" + data.info.num_results + " albums"
+          title: "" + data.info.num_results + " artists"
         });
       });
       return false;
