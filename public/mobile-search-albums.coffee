d44278b0a41dd28b694a3f7135c116241d62f565
Mark Ryall <mark@ryall.name>
2013-01-09 21:25:04 +1000

    added album search

--- public/mobile-search-albums.coffee
+++ public/mobile-search-albums.coffee
@@ -1,9 +1,29 @@
 $ ->
+  template = """
+  <p>{{info.num_results}} albums</p>
+  <ul id="albums-list" data-role="listview" data-split-icon="plus" data-split-theme="d">
+    {{#albums}}
+      <li>
+        <a href="#">
+          <img src="{{icon}}" class="ui-li-thumb">
+          <h3 class="ui-li-heading">{{name}}</h3>
+          <p class="ui-li-desc">{{artists}}</p>
+          <p class="ui-li-desc">{{date}} - {{count}} tracks - {{duration}}</p>
+        </a>
+        <a href="#" class='plus' data-track-id="{{id}}">add</a>
+      </li>
+    {{/albums}}
+  </ul>
+  """
+
   $('#search-albums-form').submit ->
     $.mobile.loading 'show'
     $.get '/api/search/albums',
       criteria: $('#search-albums-criteria').val(),
       (data) ->
         $.mobile.loading 'hide'
-        console.log data
+        content = Mustache.to_html template, data
+        $('#albums .albums-content').html content
+        $.mobile.changePage '#albums'
+        $('#albums-list').listview()
     false
--- public/mobile-search-albums.js
+++ public/mobile-search-albums.js
@@ -1,13 +1,19 @@
 (function() {
 
   $(function() {
+    var template;
+    template = "<p>{{info.num_results}} albums</p>\n<ul id=\"albums-list\" data-role=\"listview\" data-split-icon=\"plus\" data-split-theme=\"d\">\n  {{#albums}}\n    <li>\n      <a href=\"#\">\n        <img src=\"{{icon}}\" class=\"ui-li-thumb\">\n        <h3 class=\"ui-li-heading\">{{name}}</h3>\n        <p class=\"ui-li-desc\">{{artists}}</p>\n        <p class=\"ui-li-desc\">{{date}} - {{count}} tracks - {{duration}}</p>\n      </a>\n      <a href=\"#\" class='plus' data-track-id=\"{{id}}\">add</a>\n    </li>\n  {{/albums}}\n</ul>";
     return $('#search-albums-form').submit(function() {
       $.mobile.loading('show');
       $.get('/api/search/albums', {
         criteria: $('#search-albums-criteria').val()
       }, function(data) {
+        var content;
         $.mobile.loading('hide');
-        return console.log(data);
+        content = Mustache.to_html(template, data);
+        $('#albums .albums-content').html(content);
+        $.mobile.changePage('#albums');
+        return $('#albums-list').listview();
       });
       return false;
     });
